470da502e032153b30eb504bcbd74a1449dc48d6
YyzclYang <YyzclYang@gmail.com>
2019-06-07 22:41:05 +0800

    feat(配置)：添加 html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'production',
   entry: {
@@ -16,5 +18,11 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'yu-ui',
+      template: 'index.html'
+    })
+  ]
 };
