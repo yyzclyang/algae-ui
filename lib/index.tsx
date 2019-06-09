eec61929c6dc140a99c543518eb3ba1f55ddb4ef
YyzclYang <YyzclYang@gmail.com>
2019-06-10 00:05:14 +0800

    feat(svg)：添加 svg-sprite-loader

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,1 @@
-import Button from './button';export { Button };
+import React from 'react';import ReactDOM from 'react-dom';import Icon from './Icon';ReactDOM.render(<Icon name='wechat' />, document.getElementById('root'));
