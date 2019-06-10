a5f1d7439c4458b4432177ac69d1380f7dcb8127
YyzclYang <YyzclYang@gmail.com>
2019-06-10 22:43:42 +0800

    feat(Icon)：添加点击事件

--- lib/Icon/Icon.tsx
+++ lib/Icon/Icon.tsx
@@ -4,10 +4,11 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick: React.MouseEventHandler<SVGElement>;
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props: IconProps) => (
-  <svg className="yu-ui-icon">
+  <svg className="yu-ui-icon" onClick={props.onClick}>
     <use xlinkHref={`#${props.name}`} />
   </svg>
 );
