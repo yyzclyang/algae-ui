397b2344301a0b4a21630ce7f7419e71f0aa5f93
YyzclYang <YyzclYang@gmail.com>
2019-06-12 21:50:18 +0800

    feat(测试)：完成 Icon 的两个单元测试（UI 和点击）

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import * as renderer from 'react-test-renderer';
 import Icon from '../index';
+import { mount } from 'enzyme';
 
 describe('icon', () => {
-  it('name 为 wechat 的 Icon', () => {
+  it('渲染一个 name 为 wechat 的 Icon', () => {
     const json = renderer.create(<Icon name="wechat" />).toJSON();
     expect(json).toMatchSnapshot();
+  });
+
+  it('响应点击事件', () => {
+    const fn = jest.fn();
+    const component = mount(<Icon name="wechat" onClick={fn} />);
+    component.find('svg').simulate('click');
+    expect(fn).toBeCalled();
   });
 });
