60b3e333cf1179ce7e3ee028a23b984c4578c001
YyzclYang <YyzclYang@gmail.com>
2019-06-19 23:13:27 +0800

    fix(heplers)：调整变量名

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -1,5 +1,5 @@
-const classes = (...names: Array<string | undefined>) => {
-  return names.filter(Boolean).join(' ');
+const classes = (...classNames: Array<string | undefined>) => {
+  return classNames.filter(Boolean).join(' ');
 };
 
 export default classes;
