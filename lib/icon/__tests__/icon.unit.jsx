02d059ab9d5be902f5606b0de9230db109085cee
YyzclYang <YyzclYang@gmail.com>
2019-06-20 23:15:15 +0800

    feat(test)：添加 Icon 的自定义 style 和 rotate 的测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -4,15 +4,33 @@ import Icon from '../index';
 import { mount } from 'enzyme';
 
 describe('icon', () => {
-  it('渲染一个 name 为 wechat 的 Icon', () => {
-    const json = renderer.create(<Icon name="wechat" />).toJSON();
+  it('渲染一个 type 为 wechat 的 Icon', () => {
+    const json = renderer.create(<Icon type="wechat" />).toJSON();
     expect(json).toMatchSnapshot();
   });
 
   it('响应点击事件', () => {
     const fn = jest.fn();
-    const component = mount(<Icon name="wechat" onClick={fn} />);
+    const component = mount(<Icon type="wechat" onClick={fn} />);
     component.find('svg').simulate('click');
     expect(fn).toBeCalled();
+  });
+
+  it('渲染自定义 style', () => {
+    const component = mount(
+      <Icon type="wechat" style={{ height: '40px', width: '40px' }} />
+    );
+    expect(component.find('svg').get(0).props.style).toHaveProperty(
+      'height',
+      '40px'
+    );
+  });
+
+  it('渲染自定义 rotate', () => {
+    const component = mount(<Icon type="wechat" rotate={180} />);
+    expect(component.find('svg').get(0).props.style).toHaveProperty(
+      'transform',
+      'rotate(180deg)'
+    );
   });
 });
