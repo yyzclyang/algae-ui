03932c9306146017d42d9cf5b6b11b6a55a2d6c9
YyzclYang <YyzclYang@gmail.com>
2019-06-21 22:52:39 +0800

    feat(Button)：完成基础样式

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,3 +1,34 @@
 import React from 'react';
+import './button.scss';
+import classes from 'ROOT/lib/helpers/classes';
 
-export default () => <div>button</div>;
+interface ButtonProps {
+  type?: 'default' | 'primary' | 'danger' | 'ghost';
+  className?: string;
+  style?: React.CSSProperties;
+  htmlType?: 'submit' | 'button' | 'reset';
+  children?: React.ReactText;
+}
+
+const Button: React.FunctionComponent<ButtonProps> = (props: ButtonProps) => {
+  const {
+    type = 'default',
+    className,
+    htmlType = 'button',
+    style,
+    children,
+    ...restProps
+  } = props;
+  return (
+    <button
+      className={classes('algae-ui-button', type, className)}
+      type={htmlType}
+      style={{ ...style }}
+      {...restProps}
+    >
+      <span>{children}</span>
+    </button>
+  );
+};
+
+export default Button;
