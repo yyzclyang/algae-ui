6595a11d9a30218c8324af5498eb24f6cb216623
YyzclYang <YyzclYang@gmail.com>
2019-06-22 19:18:36 +0800

    feat(Button)：新增 full loading 属性

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,28 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Icon from '../icon';
 import './button.scss';
 import classNames from '../helpers/classNames';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   buttonType?: 'default' | 'primary' | 'danger';
-  className?: string;
   icon?: string;
+  loading?: boolean;
   ghost?: boolean;
-  children?: React.ReactText;
+  full?: boolean;
+  children?: React.ReactNode;
 }
 
 const Button: React.FunctionComponent<ButtonProps> = (props: ButtonProps) => {
-  const { buttonType, className, icon, ghost, children, ...restProps } = props;
+  const {
+    buttonType,
+    className,
+    onClick,
+    icon,
+    loading,
+    ghost,
+    full,
+    children,
+    ...restProps
+  } = props;
+  const handleClick: React.MouseEventHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    onClick && onClick(e);
+  };
   return (
     <button
       className={classNames(
         'algae-ui-button',
-        buttonType,
+        `algae-ui-button-${buttonType}`,
         className,
-        ghost ? 'ghost' : undefined
+        ghost ? 'algae-ui-button-ghost' : undefined,
+        full ? 'algae-ui-button-full' : undefined
       )}
+      onClick={handleClick}
       {...restProps}
     >
+      {loading && (
+        <Icon className={'algae-ui-icon-loading'} type="loading"></Icon>
+      )}
       {icon && <Icon type="wechat"></Icon>}
       {children}
     </button>
@@ -31,7 +53,18 @@ const Button: React.FunctionComponent<ButtonProps> = (props: ButtonProps) => {
 
 Button.defaultProps = {
   buttonType: 'default',
+  loading: false,
   ghost: false
+};
+
+Button.propTypes = {
+  buttonType: PropTypes.oneOf(['default', 'primary', 'danger']),
+  icon: PropTypes.string,
+  loading: PropTypes.bool,
+  ghost: PropTypes.bool,
+  onClick: PropTypes.func,
+  full: PropTypes.bool,
+  children: PropTypes.node
 };
 
 export default Button;
