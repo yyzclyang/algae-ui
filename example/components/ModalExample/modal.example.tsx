171cd3aa6498326be2f0327e7cc5a61f5fde343f
YyzclYang <YyzclYang@gmail.com>
2019-06-25 23:39:52 +0800

    fix(Example)：更改展示用例

--- example/components/ModalExample/modal.example.tsx
+++ example/components/ModalExample/modal.example.tsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
 import { Modal } from 'ROOT/src';
-import Button from 'ROOT/src/button';
+import { Button } from 'ROOT/src';
 
 const ModalExample: React.FunctionComponent = () => {
   const [visible, setVisible] = useState<boolean>(false);
   return (
     <div>
-      <Button onClick={() => setVisible(!visible)}>Modal</Button>
-      {visible && <Modal visible>Modal Message</Modal>}
+      <Button onClick={() => setVisible(true)}>Modal</Button>
+      {visible && (
+        <Modal
+          visible
+          onClose={() => setVisible(false)}
+          buttons={[
+            <Button key="btn1" ghost onClick={() => setVisible(false)}>
+              cancel
+            </Button>,
+            <Button
+              key="btn2"
+              buttonType="success"
+              onClick={() => setVisible(false)}
+            >
+              OK
+            </Button>
+          ]}
+        >
+          Modal Message
+        </Modal>
+      )}
     </div>
   );
 };
