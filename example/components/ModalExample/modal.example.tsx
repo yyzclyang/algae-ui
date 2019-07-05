1bfb3fa5d6aa5d55bfb6da5a5e028e9d7421e86c
YyzclYang <YyzclYang@gmail.com>
2019-07-05 16:52:45 +0800

    feat(Example)：添加几个 Modal 的例子

--- example/components/ModalExample/modal.example.tsx
+++ example/components/ModalExample/modal.example.tsx
@@ -30,20 +30,127 @@ const ModalExample: React.FunctionComponent = () => {
       )}
       <Button
         buttonType="primary"
-        onClick={() =>
-          confirm({
-            content: 'confirm',
+        onClick={() => {
+          const modal = confirm({
+            title: 'This is a notification message',
+            content: (
+              <div>
+                <p>some messages...some messages...</p>
+                <p>some messages...some messages...</p>
+              </div>
+            ),
             iconType: 'like',
-            onOk: () => {
-              console.log('你点击了 OK');
+            okButton: {
+              content: 'Ok',
+              onClick: () => {
+                console.log('你点击了 OK');
+              }
             },
-            onCancel: () => {
-              console.log('你点击了 cancel');
+            cancelButton: {
+              content: 'Cancel',
+              onClick: () => {
+                console.log('你点击了 Cancel');
+              }
             }
-          })
-        }
+          });
+          setTimeout(() => {
+            modal.update({
+              title: 'update',
+              content: (
+                <div>
+                  <p>some messages has updated...</p>
+                  <p>some messages has updated...</p>
+                </div>
+              )
+            });
+          }, 2000);
+        }}
       >
         confirm
+      </Button>
+      <Button
+        onClick={() => {
+          Modal.alert({
+            title: 'This is a notification message',
+            content: (
+              <div>
+                <p>some messages...some messages...</p>
+                <p>some messages...some messages...</p>
+              </div>
+            ),
+            iconType: 'like',
+            okButton: {
+              content: 'Ok',
+              onClick: (e) => {
+                console.log('你点击了 OK');
+              }
+            },
+            cancelButton: {
+              content: 'Cancel',
+              onClick: (e) => {
+                console.log('你点击了 Cancel');
+              }
+            }
+          });
+        }}
+      >
+        Modal.alert
+      </Button>
+      <Button
+        buttonType="primary"
+        onClick={() => {
+          Modal.info({
+            title: 'this is title',
+            content: 'some message',
+            onClick: (e) => {
+              console.log(e);
+            }
+          });
+        }}
+      >
+        Modal.info
+      </Button>
+      <Button
+        buttonType="success"
+        onClick={() => {
+          Modal.success({
+            title: 'this is title',
+            content: 'some message',
+            onClick: (e) => {
+              console.log(e);
+            }
+          });
+        }}
+      >
+        Modal.success
+      </Button>
+      <Button
+        buttonType="danger"
+        onClick={() => {
+          Modal.error({
+            title: 'this is title',
+            content: 'some message',
+            onClick: (e) => {
+              console.log(e);
+            }
+          });
+        }}
+      >
+        Modal.error
+      </Button>
+      <Button
+        buttonType="default"
+        onClick={() => {
+          Modal.warning({
+            title: 'this is title',
+            content: 'some message',
+            onClick: (e) => {
+              console.log(e);
+            }
+          });
+        }}
+      >
+        Modal.warning
       </Button>
     </div>
   );
