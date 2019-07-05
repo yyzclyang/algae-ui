989ca6f4b097bd281ae1cdb942f062a02fda9f69
YyzclYang <YyzclYang@gmail.com>
2019-07-05 21:21:29 +0800

    test(Modal)：完善 Modal 的单元测试

--- src/modal/__tests__/modal.unit.jsx
+++ src/modal/__tests__/modal.unit.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
-import Modal from '../index';
-import Button from '../../button';
 import { mount } from 'enzyme';
+import Modal, { confirm } from '../index';
+import Button from '../../button';
 
 let visible = true;
 const onClose = () => (visible = false);
@@ -10,6 +10,32 @@ const modal = (
   <Modal
     visible={visible}
     closeOnClickMask
+    onClose={onClose}
+    buttons={[
+      <Button id="btn1" key="btn1" ghost onClick={onClose}>
+        Cancel
+      </Button>,
+      <Button id="btn2" key="btn2" buttonType="success" onClick={onClose}>
+        OK
+      </Button>
+    ]}
+  >
+    Modal Message
+  </Modal>
+);
+
+const modalWithInvisible = (
+  <Modal visible={false} onClose={onClose} className="modal-with-invisible">
+    Modal Message
+  </Modal>
+);
+
+const modalWithoutClosable = (
+  <Modal
+    className="modal-without-closable"
+    visible={visible}
+    closeOnClickMask
+    closable={false}
     onClose={onClose}
     buttons={[
       <Button id="btn1" key="btn1" ghost onClick={onClose}>
@@ -32,6 +58,31 @@ describe('Modal', () => {
       1
     );
     expect(document.body.querySelectorAll('.algae-ui-dialog').length).toBe(1);
+    ReactTestUtils.Simulate.click(
+      document.querySelectorAll('.algae-ui-close')[0]
+    );
+  });
+
+  it('渲染一个 modalWithInvisible 的 Modal', () => {
+    mount(modalWithInvisible);
+    expect(
+      document.body.querySelectorAll(
+        '.algae-ui-dialog-mask.modal-with-invisible'
+      ).length
+    ).toBe(0);
+    expect(
+      document.body.querySelectorAll('.algae-ui-dialog.modal-with-invisible')
+        .length
+    ).toBe(0);
+  });
+
+  it('渲染一个没有关闭 Icon 的 Modal', () => {
+    mount(modalWithoutClosable);
+    expect(
+      document.body.querySelectorAll(
+        '.algae-ui-dialog.modal-without-closable > .algae-ui-header > .algae-ui-close'
+      ).length
+    ).toBe(0);
   });
 
   it('点击 Cancel 按钮 Modal 消失', () => {
@@ -80,5 +131,267 @@ describe('Modal', () => {
       ).toBe(0);
       expect(document.body.querySelectorAll('.algae-ui-dialog').length).toBe(0);
     }, 100);
+  });
+
+  it('Modal.alert', () => {
+    const button = (
+      <Button
+        id="alert-test"
+        onClick={() => {
+          Modal.alert({
+            title: 'This is a notification message',
+            content: (
+              <div>
+                <p>some messages...some messages...</p>
+                <p>some messages...some messages...</p>
+              </div>
+            ),
+            iconType: 'like',
+            okButton: {
+              content: 'Ok',
+              onClick: () => {}
+            },
+            cancelButton: {
+              content: 'Cancel',
+              onClick: () => {}
+            }
+          });
+        }}
+      >
+        Modal.alert
+      </Button>
+    );
+    const buttonDom = mount(button);
+    buttonDom.find('button').simulate('click');
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+    ).toBe(1);
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+    ).toBe(1);
+    ReactTestUtils.Simulate.click(
+      document.querySelectorAll('.algae-ui-confirm-dialog .algae-ui-button')[0]
+    );
+    setTimeout(() => {
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+      ).toBe(0);
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+      ).toBe(0);
+    }, 100);
+  });
+
+  it('Modal.info', () => {
+    const button = (
+      <Button
+        buttonType="primary"
+        onClick={() => {
+          Modal.info({
+            title: 'this is title',
+            content: 'some message',
+            onClick: () => {}
+          });
+        }}
+      >
+        Modal.info
+      </Button>
+    );
+    const buttonDom = mount(button);
+    buttonDom.find('button').simulate('click');
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+    ).toBe(1);
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+    ).toBe(1);
+    ReactTestUtils.Simulate.click(
+      document.querySelectorAll('.algae-ui-confirm-dialog .algae-ui-button')[0]
+    );
+    setTimeout(() => {
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+      ).toBe(0);
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+      ).toBe(0);
+    }, 100);
+  });
+
+  it('Modal.success', () => {
+    const button = (
+      <Button
+        buttonType="success"
+        onClick={() => {
+          Modal.success({
+            title: 'this is title',
+            content: 'some message',
+            onClick: () => {}
+          });
+        }}
+      >
+        Modal.success
+      </Button>
+    );
+    const buttonDom = mount(button);
+    buttonDom.find('button').simulate('click');
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+    ).toBe(1);
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+    ).toBe(1);
+    ReactTestUtils.Simulate.click(
+      document.querySelectorAll('.algae-ui-confirm-dialog .algae-ui-button')[0]
+    );
+    setTimeout(() => {
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+      ).toBe(0);
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+      ).toBe(0);
+    }, 100);
+  });
+
+  it('Modal.error', () => {
+    const button = (
+      <Button
+        buttonType="danger"
+        onClick={() => {
+          Modal.error({
+            title: 'this is title',
+            content: 'some message',
+            onClick: () => {}
+          });
+        }}
+      >
+        Modal.error
+      </Button>
+    );
+    const buttonDom = mount(button);
+    buttonDom.find('button').simulate('click');
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+    ).toBe(1);
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+    ).toBe(1);
+    ReactTestUtils.Simulate.click(
+      document.querySelectorAll('.algae-ui-confirm-dialog .algae-ui-button')[0]
+    );
+    setTimeout(() => {
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+      ).toBe(0);
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+      ).toBe(0);
+    }, 100);
+  });
+
+  it('Modal.warning', () => {
+    const button = (
+      <Button
+        buttonType="default"
+        onClick={() => {
+          Modal.warning({
+            title: 'this is title',
+            content: 'some message',
+            onClick: () => {}
+          });
+        }}
+      >
+        Modal.warning
+      </Button>
+    );
+    const buttonDom = mount(button);
+    buttonDom.find('button').simulate('click');
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+    ).toBe(1);
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+    ).toBe(1);
+    ReactTestUtils.Simulate.click(
+      document.querySelectorAll('.algae-ui-confirm-dialog .algae-ui-button')[0]
+    );
+    setTimeout(() => {
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+      ).toBe(0);
+      expect(
+        document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+      ).toBe(0);
+    }, 100);
+  });
+
+  it('confirm', () => {
+    const modal = confirm({
+      title: 'This is a notification message',
+      content: (
+        <div>
+          <p>some messages...some messages...</p>
+          <p>some messages...some messages...</p>
+        </div>
+      ),
+      iconType: 'like',
+      okButton: {
+        content: 'Ok',
+        onClick: () => {}
+      },
+      cancelButton: {
+        content: 'Cancel',
+        onClick: () => {}
+      }
+    });
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+    ).toBe(1);
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+    ).toBe(1);
+
+    modal.update({ title: 'title', content: 'content' });
+    expect(
+      document.body.querySelectorAll('.confirm-dialog-message > .title')[0]
+        .innerHTML
+    ).toBe('title');
+    expect(
+      document.body.querySelectorAll('.confirm-dialog-message > .content')[0]
+        .innerHTML
+    ).toBe('content');
+
+    modal.destroy();
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog-mask').length
+    ).toBe(0);
+    expect(
+      document.body.querySelectorAll('.algae-ui-confirm-dialog').length
+    ).toBe(0);
+  });
+
+  it('confirm 只接受一个 button', () => {
+    const modal = confirm({
+      title: 'This is a notification message',
+      content: (
+        <div>
+          <p>some messages...some messages...</p>
+          <p>some messages...some messages...</p>
+        </div>
+      ),
+      iconType: 'like',
+      cancelButton: {
+        content: 'Cancel',
+        onClick: () => {}
+      }
+    });
+    expect(
+      document.body.querySelectorAll(
+        '.algae-ui-confirm-dialog .algae-ui-button'
+      ).length
+    ).toBe(1);
+
+    modal.destroy();
   });
 });
