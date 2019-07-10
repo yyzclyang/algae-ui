b8b1ed6de813f6ea1a3d9d91e71260ccbfb840ae
YyzclYang <YyzclYang@gmail.com>
2019-07-10 11:04:36 +0800

    feat(Icon)：支持 iconfont.cn 的 Symbol url 添加 Icon

--- src/icon/icon.tsx
+++ src/icon/icon.tsx
@@ -4,14 +4,14 @@ import './importIcons';
 import './icon.scss';
 import classNames from '../utils/classNames';
 
-interface IconProps extends React.SVGAttributes<SVGElement> {
+interface SvgProps extends React.SVGAttributes<SVGElement> {
   type?: string;
   component?: React.ReactElement;
   style?: React.CSSProperties;
   rotate?: number;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props: IconProps) => {
+const Svg: React.FunctionComponent<SvgProps> = (props: SvgProps) => {
   const { className, type, component, style, rotate, ...restProps } = props;
   const rotateStyle = rotate ? { transform: `rotate(${rotate}deg)` } : {};
 
@@ -32,13 +32,39 @@ const Icon: React.FunctionComponent<IconProps> = (props: IconProps) => {
   );
 };
 
-Icon.propTypes = {
-  type: PropTypes.string,
-  component: PropTypes.element,
-  rotate: PropTypes.number,
-  style: PropTypes.object
-};
+Svg.displayName = 'SvgIcon';
 
-Icon.displayName = 'Icon';
+interface IconProps extends SvgProps {}
+
+interface CreateFromIconfontCNProps {
+  scriptUrl: string;
+}
+
+class Icon extends React.Component<IconProps> {
+  static propTypes = {
+    type: PropTypes.string,
+    component: PropTypes.element,
+    rotate: PropTypes.number,
+    style: PropTypes.object
+  };
+  static displayName = 'Icon';
+
+  static createFromIconfontCN: (
+    props: CreateFromIconfontCNProps
+  ) => React.FunctionComponent<SvgProps>;
+
+  render() {
+    return <Svg {...this.props} />;
+  }
+}
+
+Icon.createFromIconfontCN = ({ scriptUrl }) => {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = scriptUrl;
+  document.body.appendChild(script);
+
+  return Svg;
+};
 
 export default Icon;
