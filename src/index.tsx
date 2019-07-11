9480bbf33e9265dc4cf0066922f38858913054e4
YyzclYang <YyzclYang@gmail.com>
2019-07-11 21:48:17 +0800

    feat(Layout)：添加 index 导出

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,7 @@ export { default as Icon } from './icon';
 export { default as Button } from './button';
 export { ButtonGroup } from './button';
 
+export { default as Layout } from './layout';
+export { Header, Content, Footer, Side } from './layout';
+
 export { default as Modal } from './modal';
