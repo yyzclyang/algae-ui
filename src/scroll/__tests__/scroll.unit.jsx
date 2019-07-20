73377be161bb1ca2c676202faa60cf79d5cd358b
YyzclYang <YyzclYang@gmail.com>
2019-07-20 22:44:07 +0800

    test(Scroll)：添加单元测试

---
+++ src/scroll/__tests__/scroll.unit.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Scroll from '../index';
+import { mount } from 'enzyme';
+
+describe('Scroll', () => {
+  it('渲染 Scroll', () => {
+    const component = mount(
+      <Scroll>
+        <div>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+      </Scroll>
+    );
+    expect(component.find('.algae-ui-scroll-wrapper')).toHaveLength(1);
+    expect(component.find('.algae-ui-scroll')).toHaveLength(1);
+    expect(component.find('p')).toHaveLength(3);
+  });
+
+  it('接受自定义属性', () => {
+    const component = mount(
+      <Scroll rightGap={5} scrollBarWidth={10} scrollBarColor="red">
+        <div>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+      </Scroll>
+    );
+    expect(
+      component.find('.algae-ui-scroll-bar').get(0).props.style
+    ).toHaveProperty('right', '5px');
+  });
+
+  it('响应 onScroll 事件', () => {
+    const fn = jest.fn();
+    const component = mount(
+      <Scroll onScroll={fn}>
+        <div>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+      </Scroll>
+    );
+    component.find('.algae-ui-scroll').simulate('scroll');
+    expect(fn).toBeCalled();
+  });
+});
