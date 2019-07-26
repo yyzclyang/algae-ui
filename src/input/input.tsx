38eb10d0d0836ec1b8657e078efd4a2b38b1504b
YyzclYang <YyzclYang@gmail.com>
2019-07-26 22:15:36 +0800

    fix(Input)：解决变成受控组件引起的问题

--- src/input/input.tsx
+++ src/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from '../index';
 import classNames from '../utils/classNames';
@@ -8,39 +8,21 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   value?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
   allowClear?: boolean;
+  clearFun?: React.MouseEventHandler<SVGAElement>;
 }
 
 const Input: React.FunctionComponent<InputProps> = (props: InputProps) => {
-  const { className, value, onChange, allowClear, ...rest } = props;
-
-  const [inputValue, setInputValue] = useState<string>('');
-
-  const inputChange: React.ChangeEventHandler = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setInputValue(value || e.target.value);
-    onChange && onChange(e);
-  };
-
-  const inputClear: React.MouseEventHandler = () => {
-    setInputValue('');
-  };
+  const { className, allowClear, clearFun, ...rest } = props;
 
   return (
     <div className="algae-ui-input-wrapper">
       <input
         type="text"
         className={classNames('algae-ui-input', className)}
-        onChange={inputChange}
-        value={inputValue}
         {...rest}
       />
-      {allowClear && inputValue && (
-        <Icon
-          type="clear"
-          className="algae-ui-input-icon"
-          onClick={inputClear}
-        />
+      {allowClear && (
+        <Icon type="clear" className="algae-ui-input-icon" onClick={clearFun} />
       )}
     </div>
   );
