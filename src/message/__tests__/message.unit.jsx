9715b4924bf2a8b8774f0e13dae6494d734cc773
YyzclYang <YyzclYang@gmail.com>
2019-09-06 13:44:11 +0800

    test(message)：修改单元测试

--- src/message/__tests__/message.unit.jsx
+++ src/message/__tests__/message.unit.jsx
@@ -2,34 +2,37 @@ import React from 'react';
 import message from '../index';
 
 describe('message', () => {
-  it('message.info', function(done) {
+  it('message.info', function() {
+    jest.useFakeTimers();
     message.info('info', 500);
     expect(document.body.querySelectorAll('.algae-ui-message').length).toBe(1);
     setTimeout(() => {
       expect(document.body.querySelectorAll('.algae-ui-message').length).toBe(
         0
       );
-      done();
     }, 2000);
+    jest.runAllTimers();
   });
 
-  it('带回调的 message', (done) => {
+  it('带回调的 message', () => {
+    jest.useFakeTimers();
     const fn = jest.fn();
     message.loading('loading', 1000, fn);
     setTimeout(() => {
       expect(fn).toBeCalled();
-      done();
     }, 1500);
+    jest.runAllTimers();
   });
 
-  it('手动销毁 message', (done) => {
+  it('手动销毁 message', () => {
+    jest.useFakeTimers();
     const destroy = message.loading('loading', 0);
     destroy();
     setTimeout(() => {
       expect(document.body.querySelectorAll('.algae-ui-message').length).toBe(
         0
       );
-      done();
     }, 500);
+    jest.runAllTimers();
   });
 });
