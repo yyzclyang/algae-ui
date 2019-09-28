3c4d9359199622dcf6093115885f81940611034e
YyzclYang <YyzclYang@gmail.com>
2019-09-28 14:38:07 +0800

    test(Form)：添加 Form 单元测试

---
+++ src/form/__tests__/form.unit.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import * as renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import Form from '../index';
+import Button from '../../button';
+
+const getForm = (onChange, onSubmit) => {
+  const formData = {
+    username: ''
+  };
+  const fields = [
+    {
+      type: 'username',
+      label: '用户名',
+      input: { type: 'text' },
+      rules: [
+        {
+          type: 'required',
+          match: true,
+          message: '不能为空'
+        }
+      ]
+    }
+  ];
+  return (
+    <Form
+      value={formData}
+      fields={fields}
+      buttons={
+        <Button full type="submit" buttonType="primary">
+          提交
+        </Button>
+      }
+      onSubmit={onSubmit}
+      onChange={onChange}
+    />
+  );
+};
+
+describe('Form', () => {
+  it('渲染一个 Form', () => {
+    const component = renderer.create(getForm(() => {}, () => {})).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Form 输入时触发 onChange，点击提交时触发 onSubmit', (done) => {
+    const onChange = jest.fn();
+    const onSubmit = jest.fn();
+
+    const form = mount(getForm(onChange, onSubmit));
+    form.find('input').simulate('change');
+    expect(onChange).toBeCalled();
+    form.find('button').simulate('click');
+    setTimeout(() => {
+      expect(onChange).toBeCalled();
+      done();
+    }, 10);
+  });
+});
