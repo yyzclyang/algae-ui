bac7a1de7469ad44009c097dd049b062735180d9
yyzcl <yyzclyang@gmail.com>
2019-11-02 23:27:08 +0800

    feat(Steps)：完善 Steps

--- src/steps/steps.tsx
+++ src/steps/steps.tsx
@@ -7,16 +7,55 @@ const sc = scopedClassMaker('algae-ui-steps');
 
 interface StepsProps {
   className?: string;
+  current?: number;
+  status?: 'waiting' | 'process' | 'success' | 'fail';
+  onChange?: (current: number) => void;
   children: React.ReactNode;
 }
 
 const Steps: React.FunctionComponent<StepsProps> = (props: StepsProps) => {
-  const { className, children } = props;
-  return <div className={classNames(sc(), sc(className))}>{children}</div>;
+  const { className, current, status, onChange, children } = props;
+
+  return (
+    <div className={classNames(sc(), className)}>
+      {React.Children.map(children, (child, index) => {
+        return current! < index
+          ? React.cloneElement(child as React.ReactElement, {
+              status: 'waiting',
+              defaultIcon: String(index + 1),
+              onClick: () => {
+                onChange && onChange(index);
+              }
+            })
+          : current === index
+          ? React.cloneElement(child as React.ReactElement, {
+              status: status || 'process',
+              defaultIcon: String(index + 1),
+              onClick: () => {
+                onChange && onChange(index);
+              }
+            })
+          : React.cloneElement(child as React.ReactElement, {
+              status: 'success',
+              defaultIcon: String(index + 1),
+              onClick: () => {
+                onChange && onChange(index);
+              }
+            });
+      })}
+    </div>
+  );
 };
 Steps.displayName = 'Steps';
 Steps.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  current: PropTypes.number,
+  status: PropTypes.oneOf(['waiting', 'process', 'success', 'fail']),
+  onChange: PropTypes.func,
+  children: PropTypes.node
+};
+Steps.defaultProps = {
+  current: 0
 };
 
 export default Steps;
