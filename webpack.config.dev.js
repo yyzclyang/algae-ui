2fc38333331b23202c4e98d69a86fa2bcadf20dd
yyzcl <yyzclyang@gmail.com>
2020-03-22 14:29:00 +0800

    chore(dev): 添加开发环境的热更新

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,15 +1,19 @@
-const baseConfig = require('./webpack.config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config');
 
-module.exports = Object.assign({}, baseConfig, {
+module.exports = merge(baseConfig, {
   mode: 'development',
-  entry: {
-    example: './example/index.tsx'
-  },
+  entry: { hot: 'react-hot-loader/patch', example: './example/index.tsx' },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'algae-ui',
       template: './example/index.html'
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
 });
