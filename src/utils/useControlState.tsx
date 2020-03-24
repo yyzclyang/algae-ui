2cfd359d3fce3cfebc10d7476882c9f0f4b541d3
yyzcl <yyzclyang@gmail.com>
2020-03-24 22:43:18 +0800

    refactor(useControlState): 修改参数名

--- src/utils/useControlState.tsx
+++ src/utils/useControlState.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import useUpdate from './useUpdate';
 
 const useControlState: <S>(
-  initialState: S | (() => S),
+  defaultState: S | (() => S),
   controlState?: S | (() => S)
 ) => [S, React.Dispatch<React.SetStateAction<S>>] = (
   initialState,
