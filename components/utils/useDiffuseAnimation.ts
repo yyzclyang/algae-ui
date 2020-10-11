02f9d6c25015935aec57061729143d69a0d74f59
yyzcl <yyzclyang@gmail.com>
2020-10-11 12:39:44 +0800

    chore(components): 修正打包的 scss 引用

--- components/utils/useDiffuseAnimation.ts
+++ components/utils/useDiffuseAnimation.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import '../style/diffuseAnimation.scss';
 
 const useDiffuseAnimation: () => [string, () => void] = () => {
   const [animationClassName, setAnimationClassName] = useState<string>('');
